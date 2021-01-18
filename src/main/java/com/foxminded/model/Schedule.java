f0a8ec7f7b6863ba5e260902ffd83bd648590472
Gleb_Kostenko <glebkostenko35@gmail.com>
2021-01-18 20:20:45 +0300

    Model

---
+++ src/main/java/com/foxminded/model/Schedule.java
@@ -0,0 +1,45 @@
+package com.foxminded.model;
+
+import java.time.LocalDateTime;
+
+public class Schedule {
+    private Group group;
+    private LocalDateTime dateTime;
+    private String duration;
+    private Teacher teacher;
+    private LectureHall lectureHall;
+    private Subject subject;
+
+    public Schedule(Group group, LocalDateTime dateTime, String duration, Teacher teacher, LectureHall lectureHall, Subject subject) {
+        this.group = group;
+        this.dateTime = dateTime;
+        this.duration = duration;
+        this.teacher = teacher;
+        this.lectureHall = lectureHall;
+        this.subject = subject;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public LectureHall getLectureHall() {
+        return lectureHall;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+}
