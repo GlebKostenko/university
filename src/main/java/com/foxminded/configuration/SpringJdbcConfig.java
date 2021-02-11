d7895dabfa345c891d265a734a2e89607f508e6a
Gleb_Kostenko <glebkostenko35@gmail.com>
2021-02-11 19:42:01 +0300

    Jndi

--- src/main/java/com/foxminded/configuration/SpringJdbcConfig.java
+++ src/main/java/com/foxminded/configuration/SpringJdbcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 import javax.sql.DataSource;
 
@@ -13,21 +13,11 @@ import javax.sql.DataSource;
 @ComponentScan("com.foxminded.dao")
 @PropertySource("classpath:app.properties")
 public class SpringJdbcConfig {
-    @Value("${url}")
-    private String url;
-    @Value("${user}")
-    private String user;
-    @Value("${password}")
-    private String password;
-    @Value("${driver}")
-    private String driver;
+    @Value("${spring.datasource.jndi-name}")
+    private String jndiDataSource;
     @Bean
     public DataSource postgresDataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driver);
-        dataSource.setUrl(url);
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
-        return dataSource;
+        JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+        return lookup.getDataSource(jndiDataSource);
     }
 }
